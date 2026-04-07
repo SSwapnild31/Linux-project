41bd30d9f2b643eca5489e2fe07833802fb6676f
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-04-07 21:51:00 +0530

    Add files via upload

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
 
-int main(int argc, char* argv[]){
-    
+#define SIZE 50
+
+void* execute_cmd(void *arg){
+    system((char*)arg);
+}
+
+int main()
+{
+    while(1){
+    	pthread_t t1;
+    	char cmd[SIZE];
+
+        printf("> ");
+        scanf(" %[^\n]",cmd);
+        
+	pthread_create(&t1, NULL, execute_cmd,(void*)cmd);
+	pthread_join(t1, NULL);
+    }
 }
